afc9ad2851d93774b3cec9f0289ad04b2af7393a
bilbo290 <70652519+bilbo290@users.noreply.github.com>
2022-01-29 17:42:49 +0700

    first commit

---
+++ scripts/helpful_scripts.py
@@ -0,0 +1,59 @@
+from brownie import network, config, accounts, MockV3Aggregator
+from web3 import Web3
+
+FORKED_LOCAL_ENVIRONMENTS = ["mainnet-fork", "mainnet-fork-dev"]
+LOCAL_BLOCKCHAIN_ENVIRONMENTS = ["development", "ganache-local"]
+
+DECIMALS = 8
+STARTING_PRICE = 200000000000
+
+
+def get_account(index=None, id=None):
+    if index:
+        return accounts[index]
+
+    if id:
+        return accounts.load(id)
+
+    if (
+        network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS
+        or network.show_active() in FORKED_LOCAL_ENVIRONMENTS
+    ):
+        return accounts[0]
+
+    return accounts.add(config["wallets"]["from_key"])
+
+
+contract_to_mock = {"eth_usd_price_feed": MockV3Aggregator}
+
+
+def get_contract(contract_name):
+    """This function will grab the contract addresses from the brownie config if defined, otherwise,
+    it will deploy a mock version of that contract, and return that mock contract.
+
+        Args:
+            contract_name (string): This is the name that is referred to in the
+            brownie config and 'contract_to_mock' variable.
+        Returns:
+            brownie.network.contract.ProjectContract: The most recently deployed
+            Contract of the type specificed by the dictionary. This could be either
+            a mock or the 'real' contract on a live network.
+    """
+    contract_type = contract_to_mock[contract_name]
+    if network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        if len(contract_type) <= 0:
+            # MockV3Aggregator.length
+            deploy_mocks()
+        contract = contract_type[-1]
+
+
+DECIMALS = 8
+INITIAL_VALUE = 200000000000
+
+
+def deploy_mocks(decimals=DECIMALS, initial_value=INITIAL_VALUE):
+    account = get_account()
+    mock_price_feed = MockV3Aggregator.deploy(
+        decimals, initial_value, {"from": account}
+    )
+    print("Mocks Deployed")
